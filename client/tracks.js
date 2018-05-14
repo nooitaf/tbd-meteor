772e675f3095d3db39b24220c674aea3e3798717
Ralf Brandenstein <nooitaf@gmail.com>
2018-05-15 00:19:17 +0200

    fixed date check and track listing

--- client/tracks.js
+++ client/tracks.js
@@ -123,23 +123,13 @@ UI.registerHelper('showInfo', function(subId){
 })
 
 UI.registerHelper('dateIsDay1', function(d){
-  if (d==='DAY1' || d==='ALL_DAYS') return true;
-  if (!moment.isDate(d)) return true;
-  var date_day_1_start = new Date('Fri Jul 14 2017 00:00:00 GMT+0200 (CEST)')
-  var date_day_1_end   = new Date('Sat Jul 15 2017 06:00:00 GMT+0200 (CEST)')
-  return moment(d).isBetween(date_day_1_start,date_day_1_end)
+  return isDay1(d)
 })
 UI.registerHelper('dateIsDay2', function(d){
-  if (d==='ALL_DAYS'||d==='---') return true;
-  var date_day_2_start = new Date('Sat Jul 15 2017 06:00:00 GMT+0200 (CEST)')
-  var date_day_2_end   = new Date('Sun Jul 16 2017 06:00:00 GMT+0200 (CEST)')
-  return moment(d).isBetween(date_day_2_start,date_day_2_end)
+  return isDay2(d)
 })
 UI.registerHelper('dateIsDay3', function(d){
-  if (d==='ALL_DAYS'||d==='---') return true;
-  var date_day_3_start = new Date('Sun Jul 16 2017 06:00:00 GMT+0200 (CEST)')
-  var date_day_3_end   = new Date('Mon Jul 17 2017 06:00:00 GMT+0200 (CEST)')
-  return moment(d).isBetween(date_day_3_start,date_day_3_end)
+  return isDay3(d)
 })
 
 Template.toggleMinimal.events({

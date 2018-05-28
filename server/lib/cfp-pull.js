3afadacb84f940a5a7a8919c14b87bd9e6c552db
nooitaf <nooitaf@gmail.com>
2018-05-28 14:30:25 +0200

    reverting cfp git pull

--- server/lib/cfp-pull.js
+++
@@ -1,8 +0,0 @@
-
-Meteor.setInterval(function(){
-  var cfp = HTTP.get('https://raw.githubusercontent.com/albjeremias/tbdcfp/master/cfp.md')
-  if (cfp && cfp.content) {
-    var meta = Meta.findOne()
-    Meta.update({_id:meta._id},{$set:{text_cfp:cfp.content}})
-  }
-}, 1000*360)
